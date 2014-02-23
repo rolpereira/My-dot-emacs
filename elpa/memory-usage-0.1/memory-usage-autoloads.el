ac634d4ecd00b1db59c0ce79ceacde5158ddd439
Rolando Pereira <rolando_pereira@sapo.pt>
2014-02-23 15:02:24 +0000

    Delete some unused files

--- elpa/memory-usage-0.1/memory-usage-autoloads.el
+++
@@ -1,28 +0,0 @@
-;;; memory-usage-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (memory-usage) "memory-usage" "memory-usage.el"
-;;;;;;  (20198 10835))
-;;; Generated autoloads from memory-usage.el
-
-(autoload 'memory-usage "memory-usage" "\
-List all buffers and their memory usage.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("memory-usage-pkg.el") (20198 10835 348592))
-
-;;;***
-
-(provide 'memory-usage-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; memory-usage-autoloads.el ends here
--- elpa/memory-usage-0.1/memory-usage-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "memory-usage" "0.1" "Analyze the memory usage of Emacs in various ways" (quote nil))
